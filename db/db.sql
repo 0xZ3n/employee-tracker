eaba180d270eab0f93e72b318dd047c4b72c761c
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-10-29 08:23:19 -0400

    database and schema creation

---
+++ db/db.sql
@@ -0,0 +1,6 @@
+/* If we already have this database, drop it */
+DROP DATABASE IF EXISTS employees;
+
+/* Creates and opens our new database for tracking employees */
+CREATE DATABASE employees;
+USE employees;
---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+/*Table for Departments*/
+CREATE TABLE Department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+/*Table for each Employee*/
+CREATE TABLE Employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
+
+/*Table for Employee Roles*/
+CREATE TABLE Role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+/**/
+
+
+/**/
+
