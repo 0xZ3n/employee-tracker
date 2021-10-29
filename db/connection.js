d78d7928e0da4e44aa9905ae5a759bbdd56d4ad4
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-10-29 12:42:31 -0400

    connection file added

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+const mysql = ("mysql2");
+
+// Establish database connection
+const dbConnect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    port: 3306,
+    database: 'employees'
+});
